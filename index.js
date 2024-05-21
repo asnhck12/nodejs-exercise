fe3bb003343a592b92a74a4861af02eb933488f5
AH LT <ahussain93@live.co.uk>
2024-05-21 12:39:58 +0100

    Minor updates

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 http.createServer(function(req, res) {
     var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
-    console.log("filename " + filename);
 
     if (q.pathname === "/") {
         fs.readFile('index.html', function(err, data) {
@@ -18,17 +17,16 @@ http.createServer(function(req, res) {
             return res.end();
         });
     } else {
-            fs.readFile(filename, function(err, data) {
-                if (err) {
-                    res.writeHead(404, {'Content-Type': 'text/html'});
-                    return res.end('/404.html');
-                }
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write(data);
-                return res.end();
-            })
-        }}).listen(8070, () => {
-            console.log('Server is listening on port 8070');})
+        fs.readFile(filename, function(err, data) {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                return res.end('/404.html');
+            }
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        })
+    }}).listen(8070)
 //     );
 //     }
 // })
