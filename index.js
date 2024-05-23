e3e69d44e86e2d2f81a9b28805c3f43e54feddd7
AH LT <ahussain93@live.co.uk>
2024-05-23 17:05:31 +0100

    Updated with React

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8070;
 
-http.createServer(function(req, res) {
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
+app.use(express.static(__dirname));
 
-    if (q.pathname === "/") {
-        fs.readFile('index.html', function(err, data) {
-            if (err) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                return res.end('/404.html');
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            return res.end();
-        });
-    } else {
-        fs.readFile(filename, function(err, data) {
-            if (err) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                return res.end('/404.html');
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            return res.end();
-        })
-    }}).listen(8070)
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname }, (err) => {
+        if (err) {
+            res.status(404).sendFile('404.html', { root: __dirname });
+        }
+    });
+});
 
+app.get('*', (req, res) => {
+    const requestedFile = path.join(__dirname, req.path);
+    res.sendFile(requestedFile, (err) => {
+        if (err) {
+            res.status(404).sendFile('404.html', { root: __dirname });
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}!`);
+});
